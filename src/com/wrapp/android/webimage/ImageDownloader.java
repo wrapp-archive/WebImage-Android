c4d928d1b58660b621bb80ebde23ccd82b1e4ceb
Nik Reiman <nik.reiman@gmail.com>
2011-10-27 14:01:13 +0200

    Adding class stubs

---
+++ src/com/wrapp/android/webimage/ImageDownloader.java
@@ -0,0 +1,4 @@
+package com.wrapp.android.webimage;
+
+public class ImageDownloader {
+}
---
+++ src/com/wrapp/android/webimage/WebImageView.java
@@ -0,0 +1,20 @@
+package com.wrapp.android.webimage;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+@SuppressWarnings({"UnusedDeclaration"})
+public class WebImageView extends ImageView {
+  public WebImageView(Context context) {
+    super(context);
+  }
+
+  public WebImageView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public WebImageView(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+  }
+}
