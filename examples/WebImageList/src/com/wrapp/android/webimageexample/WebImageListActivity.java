fdef2c01a53c11d088b0ca0103ec5d9043ec9529
Nik Reiman <nik.reiman@gmail.com>
2011-10-31 09:25:04 +0100

    Main menu can refresh, clear caches

--- examples/WebImageList/src/com/wrapp/android/webimageexample/WebImageListActivity.java
+++ examples/WebImageList/src/com/wrapp/android/webimageexample/WebImageListActivity.java
@@ -3,6 +3,9 @@ package com.wrapp.android.webimageexample;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import com.wrapp.android.webimage.WebImage;
 
 public class WebImageListActivity extends ListActivity {
@@ -29,5 +32,37 @@ public class WebImageListActivity extends ListActivity {
   public void onLowMemory() {
     super.onLowMemory();
     WebImage.clearMemoryCaches();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater menuInflater = new MenuInflater(this);
+    menuInflater.inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch(item.getItemId()) {
+      case R.id.MainMenuRefreshItem:
+        refresh();
+        break;
+      case R.id.MainMenuClearCachesItem:
+        WebImage.cancelAllRequests();
+        WebImage.clearMemoryCaches();
+        WebImage.clearOldCacheFiles(0);
+        refresh();
+        break;
+      default:
+        refresh();
+        break;
+    }
+
+    return true;
+  }
+
+  private void refresh() {
+    final WebImageListAdapter listAdapter = (WebImageListAdapter)getListAdapter();
+    listAdapter.notifyDataSetChanged();
   }
 }
