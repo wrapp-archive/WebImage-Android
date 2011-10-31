ebb4f4cc6184f33bc70b103535bb2f7ba55e968d
Nik Reiman <nik.reiman@gmail.com>
2011-10-31 10:41:32 +0100

    Adding text field next to image
    Should put a bit more stress on the GUI for more accurate real-world
    simulations.

--- examples/WebImageList/src/com/wrapp/android/webimageexample/WebImageContainerView.java
+++ examples/WebImageList/src/com/wrapp/android/webimageexample/WebImageContainerView.java
@@ -3,15 +3,17 @@ package com.wrapp.android.webimageexample;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import com.wrapp.android.webimage.WebImageView;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 @SuppressWarnings({"UnusedDeclaration"})
-public class WebImageContainerView extends LinearLayout {
+public class WebImageContainerView extends RelativeLayout {
   private WebImageView webImageView;
+  private TextView imageText;
 
   public WebImageContainerView(Context context) {
     super(context);
@@ -23,10 +25,16 @@ public class WebImageContainerView extends LinearLayout {
     initialize(context);
   }
 
+  public WebImageContainerView(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    initialize(context);
+  }
+
   private void initialize(Context context) {
     LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     layoutInflater.inflate(R.layout.web_image_container_view, this, true);
     webImageView = (WebImageView)findViewById(R.id.WebImageView);
+    imageText = (TextView)findViewById(R.id.WebImageViewText);
   }
 
   public void setImageUrl(String imageUrlString) {
@@ -38,5 +46,9 @@ public class WebImageContainerView extends LinearLayout {
     catch(MalformedURLException e) {
       e.printStackTrace();
     }
+  }
+
+  public void setImageText(String text) {
+    imageText.setText(text);
   }
 }
