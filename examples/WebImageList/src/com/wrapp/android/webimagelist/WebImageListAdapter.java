e46a27843333269eca2160b39d0b9866d869ec7f
Nik Reiman <nik.reiman@gmail.com>
2011-12-22 09:57:01 +0100

    Use number images with random backgrounds
    This is MUCH more useful when testing to see if the listview loaded the
    correct image in a particular view.

--- examples/WebImageList/src/com/wrapp/android/webimagelist/WebImageListAdapter.java
+++ examples/WebImageList/src/com/wrapp/android/webimagelist/WebImageListAdapter.java
@@ -27,6 +27,7 @@ import android.widget.BaseAdapter;
 import com.wrapp.android.webimage.WebImageView;
 
 public class WebImageListAdapter extends BaseAdapter {
+  private static final boolean USE_NUMBER_IMAGES = true;
   private static final boolean USE_AWESOME_IMAGES = true;
   private static final int NUM_IMAGES = 100;
   private static final int IMAGE_SIZE = 100;
@@ -43,13 +44,19 @@ public class WebImageListAdapter extends BaseAdapter {
   }
 
   private String getImageUrl(int i) {
-    if(USE_AWESOME_IMAGES) {
-      // Unicorns!
-      return "http://unicornify.appspot.com/avatar/" + i + "?s=" + IMAGE_SIZE;
+    if(USE_NUMBER_IMAGES) {
+      // Numbers with random backgrounds. More useful for testing correct listview behavior
+      return "http://static.nikreiman.com/numbers/" + i + ".png";
     }
     else {
-      // Boring identicons
-      return "http://www.gravatar.com/avatar/" + i + "?s=" + IMAGE_SIZE + "&d=identicon";
+      if(USE_AWESOME_IMAGES) {
+        // Unicorns!
+        return "http://unicornify.appspot.com/avatar/" + i + "?s=" + IMAGE_SIZE;
+      }
+      else {
+        // Boring identicons
+        return "http://www.gravatar.com/avatar/" + i + "?s=" + IMAGE_SIZE + "&d=identicon";
+      }
     }
   }
 
